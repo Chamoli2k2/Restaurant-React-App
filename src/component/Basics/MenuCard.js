0cf4747e7a20419adb451c6523ecd4664f2e6259
Chamoli <godchamoli@gmail.com>
2023-03-28 01:05:24 +0530

    commit 2

--- src/component/Basics/MenuCard.js
+++ src/component/Basics/MenuCard.js
@@ -1,8 +1,7 @@
 import React from "react";
 
 const MenuCard = ({ menuData }) => {
-  //   console.log(menuData);
-
+  
   return (
     <>
       <section className="main-card--cointainer">
--- src/component/Basics/menuApi.js
+++ src/component/Basics/menuApi.js
@@ -2,9 +2,9 @@ const Menu = [
   {
     id: 1,
     image: "images/maggi.jpg",
-    name: "maggi",
+    name: "Maggi",
     category: "breakfast",
-    price: "12₹",
+    price: "₹14",
     description:
       "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
   },
@@ -12,74 +12,74 @@ const Menu = [
   {
     id: 2,
     image: "images/allupakoida.jpg",
-    name: "allu pakoida",
+    name: "Allu Pakoda",
     category: "evening",
-    price: "20₹",
+    price: "₹20",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Aloo pakoda, also known as aloo pakora, is a popular Indian snack made from sliced potatoes that are coated in a spiced chickpea flour batter and deep-fried until golden and crispy. It is a classic street food and is often served with mint chutney or tamarind chutney as a dipping sauce.",
   },
   {
     id: 3,
     image: "images/corn.jpg",
-    name: "corn",
+    name: "Corn",
     category: "breakfast",
-    price: "10₹",
+    price: "₹10",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Corn, also known as maize, is a cereal grain that is widely cultivated and consumed around the world. It is native to the Americas and has been a staple food for many indigenous peoples for thousands of years. Today, corn is grown in many countries and is used in a variety of ways, from food and feed to biofuels and industrial products.",
   },
   {
     id: 4,
     image: "../images/chola.jpg",
-    name: "chola",
+    name: "Chole-bhature",
     category: "lunch",
-    price: "50₹",
+    price: "₹50",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Chole bhatura is a popular North Indian dish that consists of two main components - chole (spiced chickpeas) and bhatura (a type of fried bread). It is a flavorful and hearty dish that is commonly eaten for breakfast, lunch, or dinner in India.",
   },
   {
     id: 5,
     image: "../images/pizza.jpg",
-    name: "pizza",
+    name: "Pizza",
     category: "evening",
-    price: "80₹",
+    price: "₹80",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Pizza is a popular Italian dish that has become a global phenomenon. It typically consists of a round, flat base of dough that is topped with tomato sauce, cheese, and a variety of other ingredients before being baked in an oven.",
   },
   {
     id: 6,
     image: "../images/nonvegthali.jpg",
     name: "Non-Veg Thali",
     category: "dinner",
-    price: "180₹",
+    price: "₹180",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "A non-vegetarian thali is a traditional Indian meal that consists of a variety of dishes made with meat, fish, or poultry. It is typically served on a large platter or tray and includes several small bowls or dishes that contain different items such as curries, rice, bread, and condiments.",
   },
   {
     id: 7,
     image: "../images/sweet.jpg",
     name: "Sweets",
     category: "dinner",
-    price: "60₹",
+    price: "₹60",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "India is famous for its diverse and delicious sweets, also known as mithai in Hindi. Indian sweets are an integral part of Indian cuisine and are served on various occasions like festivals, weddings, and other celebrations. Here are some of the most popular Indian sweets: Gulab jamun, rasmalai, barfi, kaju-katli etc",
   },
   {
     id: 8,
     image: "../images/rajmarice.jpg",
     name: "Rajma Rice",
     category: "lunch",
-    price: "60₹",
+    price: "₹60",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Rajma rice is a popular North Indian dish that consists of red kidney beans (rajma) cooked in a spicy tomato-based gravy and served with steamed rice. It is a hearty and satisfying dish that is both nutritious and delicious.",
   },
   {
     id: 9,
     image: "../images/samosa.jpg",
-    name: "samaso",
+    name: "Samaso",
     category: "evening",
-    price: "10₹",
+    price: "₹10",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+      "Samosa is a popular Indian snack that is loved by people all over the world. It is a triangular-shaped pastry that is stuffed with spiced potatoes, peas, and sometimes meat or paneer (Indian cottage cheese). The filling is then enclosed in a crispy, golden-brown crust made from flour and water dough.",
   },
 ];
 
